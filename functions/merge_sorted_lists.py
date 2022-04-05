aa8f8402c60563c4ebc1f11ac88a635cbe51c8f6
Chris McAnally <chris@adadevelopersacademy.org>
2022-04-05 15:33:33 -0700

    blank functions

--- functions/merge_sorted_lists.py
+++ functions/merge_sorted_lists.py
@@ -1,4 +1,2 @@
 def merge_sorted_lists(l1, l2):
-    merged = l1 + l2
-
-    return sorted(merged)
+    pass
--- functions/two_sum.py
+++ functions/two_sum.py
@@ -1,5 +1,2 @@
 def twoSum(n, t):
-    for i in range(len(n)):
-        for j in range(i + 1, len(n)):
-            if n[j] == t - n[i]:
-                return [i, j]
+    pass
