358d90b81c80df16ff54bf5146a237ee50bc9b2b
thaimynguyen <thaimynguyen@gmail.com>
2022-05-12 13:54:20 -0700

    addfunction palindrome_number and unit tests

--- functions/palindrome_number.py
+++ functions/palindrome_number.py
@@ -1,2 +1,12 @@
 def palindrome_number(x):
-    pass
+    temp = x
+    reverse = 0
+    while(x > 0):
+        dig = x % 10
+        reverse = reverse*10 + dig
+        x = x//10
+
+        if temp == reverse:
+            return True
+        else:
+            return False
