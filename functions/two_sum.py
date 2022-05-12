d77dd02980ae04318b8a54d01ea3b05387f6d388
Roshni Patel <rpatel01@wesleyan.edu>
2022-05-12 13:52:44 -0700

    Add two sum solution and tests

--- functions/two_sum.py
+++ functions/two_sum.py
@@ -1,2 +1,5 @@
 def twoSum(n, t):
-    pass
+    for i in range(len(n)):
+        for j in range(i + 1, len(n)):
+            if n[j] == t - n[i]:
+                return [i, j]
--- tests/two_sum_test.py
+++ tests/two_sum_test.py
@@ -1,2 +1,37 @@
 import pytest
 from functions.two_sum import twoSum
+
+def test_returns_0_1_for_2_7_11_15_with_target_9():
+    # Arrange
+    n = [2, 7, 11, 15]
+    t = 9
+
+    # Act
+    answer = twoSum(n, t)
+
+    # Assert
+    assert answer == [0, 1]
+
+
+def test_returns_1_2_for_3_2_4_with_target_6():
+    # Arrange
+    n = [3, 2, 4]
+    t = 6
+
+    # Act
+    answer = twoSum(n, t)
+
+    # Assert
+    assert answer == [1, 2]
+
+
+def test_returns_0_1_for_3_3_with_target_6():
+    # Arrange
+    n = [3, 3]
+    t = 6
+
+    # Act
+    answer = twoSum(n, t)
+
+    # Assert
+    assert answer == [0, 1]
